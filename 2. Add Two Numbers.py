5b7ac59b4dcb03b3e411d8a0181710728ec4814d
nizhan26 <43109244+nizhan26@users.noreply.github.com>
2018-11-05 10:19:44 +0000

    Update 2. Add Two Numbers.py

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -3,6 +3,9 @@
 # The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 # You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
+#Complexity
+#T: O(n1+n2)
+#S: O(n1+n2)
 class ListNode(object):
    def __init__(self, x):
        self.val = x
