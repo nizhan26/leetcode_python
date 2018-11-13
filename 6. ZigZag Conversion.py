748316772563f153bf752eaf6e60a351b4f3ab98
nizhan26 <43109244+nizhan26@users.noreply.github.com>
2018-11-13 05:49:06 +0000

    Create 6. ZigZag Conversion.py

---
+++ 6. ZigZag Conversion.py
@@ -0,0 +1,58 @@
+
+#
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+# (you may want to display this pattern in a fixed font for better legibility)
+#
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# And then read line by line: "PAHNAPLSIIGYIR"
+# Write the code that will take a string and make this conversion given a number of rows:
+#
+# string convert(string text, int nRows);
+# convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+
+
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        #Solution 1
+        # Time:  O(n^2)
+        # Space: O(1)
+        if numRows == 1:
+        return s
+        
+        n, step, zigzag = len(s), 2*numRows-2, ''
+        
+        for i in range(numRows):
+            for j in range(i, n, step):
+                zigzag += s[j]
+                if 0 < i < numRows - 1 and j+step-2*i < n:
+                    zigzag += s[j+step-2*i]
+                    
+        return zigzag
+        
+        
+        #Solution 2
+        #Time: O(n)
+        #Speace: O(1)
+        
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        L = [''] * numRows
+        index, step = 0, 1
+        
+        for x in s:
+            L[index] += x
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+            
+        return ''.join(L)
